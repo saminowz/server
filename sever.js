3958d068f568aa85d7511a07740096dec88278c3
saminowz <1959.sami.1959@gmail.com>
2017-01-30 15:00:46 -0500

    Server

--- sever.js
+++ sever.js
@@ -10,6 +10,11 @@ app.set("view engine","hbs")
 app.get("/",(req,res)=>{
   res.render("home.hbs");
 })
+app.get("/about",(req,res)=>{
+  res.render("about.hbs",{
+    message:"About page"
+  });
+})
 app.listen(port,()=>{
   console.log("Starting Server");
 })
